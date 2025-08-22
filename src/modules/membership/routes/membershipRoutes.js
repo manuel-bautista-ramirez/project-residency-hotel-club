4cb667b9719400a4eafdc5dff5981dc4aa0dc4b5
dlUNA78 <dajelu2003@gmail.com>
2025-08-21 20:07:31 -0600

    Create views of Membership
    Se crearon las interfaces de las membresias con las validaciones de ADMIN o RECEPSIONISTA

--- src/modules/membership/routes/membershipRoutes.js
+++ src/modules/membership/routes/membershipRoutes.js
@@ -1,24 +1,38 @@
 import express from 'express';
-import { renderMembershipHome, renderMembershipList} from '../controllers/membershipController.js';
+import { renderMembershipHome, renderMembershipList, renderMembershipCreate } from '../controllers/membershipController.js';
 import { authMiddleware, roleMiddleware } from '../../login/middlewares/accessDenied.js';
 import { MembershipController } from '../controllers/createMemberController.js';
 
 const routerMember = express.Router();
 
 // Primero autenticación (todos deben estar logueados)
+// Primero autenticación (todos deben estar logueados)
 routerMember.use(authMiddleware);
 
 // Rutas accesibles a TODOS los roles autenticados
+// Rutas accesibles a TODOS los roles autenticados
 routerMember.get('/', renderMembershipHome);
-routerMember.get('/listMembership', renderMembershipList);
+routerMember.get('/membershipList', renderMembershipList);
+routerMember.get('/create', renderMembershipCreate);
+// Rutas SOLO para Administrador
+routerMember.get('/edit/:id', roleMiddleware('Administrador'), (req, res) => {
+  res.send('Vista para editar membresías (solo admin)');
+});
 
-// Ruta para crear membresía que carga los tipos de membresía
-routerMember.get('/createMembership', MembershipController.renderCreate);
+routerMember.get('/delete/:id', roleMiddleware('Administrador'), (req, res) => {
+  res.send('Vista para eliminar membresías (solo admin)');
+});
+
+routerMember.get('/reports', roleMiddleware('Administrador'), (req, res) => {
+  res.send('Vista para reportes de membresías (solo admin)');
+});
 
 
-
-//post
-routerMember.post("/createClient", MembershipController.createClient);
-routerMember.post("/createMembership", MembershipController.createMembership);
 
 export { routerMember };
+
+
+
+
+
+
