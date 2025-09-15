3fa8e4e03b70a38fa898692b8612c865b0b8d05b
dlUNA78 <dajelu2003@gmail.com>
2025-09-15 09:41:07 -0600

    cambios no necesarios

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -3,13 +3,13 @@ port.config();
 
 export const config={
   app: {
-    port: process.env.APP_PORT || 3000
+    port: process.env.PORT || 3000
 
   },
   mysql:{
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || '586226',
     database: process.env.DB_NAME || 'hotel_club'
 
   },
